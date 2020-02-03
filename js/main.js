42cf8360b50fef086e56a0233a677341689cbe04
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-03 12:51:20 +0100

    I cd si mostrano e nascondono alla scelta sulla select

--- js/main.js
+++ js/main.js
@@ -5,32 +5,39 @@ $(document).ready(function() {
         method: "GET",
         success: function(data, stato) {
           printSingleCd(data.response);
-
-            $('.cd').each(function () {
-            var cdGenre = $(this).find('span.genre').text();
-            var option = $('option').attr('data-album');
-            console.log(cdGenre);
-            console.log(option);
-          });
-
         },
         error: function(richiesta, stato, errori) {
           alert('E\' avvenuto un errore.' + errori);
         }
       }
     );
+    $(document).on('change', 'select', function() {
+      if($('.cd').length > 0) {
+        filterCds($('.cd'));
+      }
+    });
 });
+function filterCds(elementsDom) {
+  elementsDom.each(function () {
+    var genre = $(this).find('.genre').text();
+    var value = $('select').val();
+    if (value != 'Choose') {
+      if(value == genre) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    } 
 
-
+  });
+}
 function printSingleCd(cds) {
   for (var i = 0; i < cds.length; i++) {
     var singleCd = cds[i];
     console.log(singleCd);
-
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
     var html = template(singleCd);
     $('.cds-container').append(html);
-
   }
 }
